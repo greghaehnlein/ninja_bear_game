db6c27227e6ffb187e235b5bc3f5d9ee0e075889
Greg Haehnlein <greghaehnlein@gmail.com>
2017-09-28 08:11:20 -0400

    first commit

---
+++ bear.rb
@@ -0,0 +1,23 @@
+require_relative 'fighter'
+
+class Bear < Fighter
+
+	def attack(enemy)
+		puts "Rawrr!!!"
+		enemy.lose_health(self.power)
+	end
+
+
+end
+
+
+# bear = Bear.new("Bear", 100, 12)
+# ninja = Fighter.new("Ninja", 100, 10)
+
+# ninja.attack(bear)
+# p bear
+
+# bear.attack(ninja)
+# p ninja
+
+
---
+++ ninja.rb
@@ -0,0 +1,17 @@
+require_relative 'fighter'
+
+class Ninja < Fighter
+
+	def attack(enemy)
+		puts ".... silence!!!"
+		enemy.lose_health(self.power)
+	end
+
+end
+
+
+# ninja = Ninja.new("Ninja", 100, 7)
+# bear = Bear.new("Bear", 110, 12)
+
+# ninja.attack(bear)
+# p bear
